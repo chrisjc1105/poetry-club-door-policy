e60b290eb7a9ef25de1f2682a7b29902501dfada
Christopher <chriscamachwork@gmail.com>
2025-04-01 12:38:20 -0400

    first draft

---
+++ script.js
@@ -0,0 +1,32 @@
+function frontDoorResponse(line) {
+    return line[0];
+}
+
+function frontDoorFullWord(line, line2, line3, line4, line5){
+    return frontDoorResponse(line) + frontDoorResponse(line2) +
+    frontDoorResponse(line3) + frontDoorResponse(line4) + 
+    frontDoorResponse(line5);
+}
+
+let fullWord = frontDoorFullWord('Stands so high', 'Huge hooves too', 
+'Impatiently waits for', 'Reins and harness', 'Eager to leave');
+
+function frontDoorPassword(fullWord) {
+    let firstLetter = fullWord[0];
+    let restOfWord = fullWord.slice(1, fullWord.length);
+
+    let finalFirstLetter = firstLetter.toUpperCase();
+    let finalRestOfWord = restOfWord.toLowerCase();
+
+    let finalWord = finalFirstLetter + finalRestOfWord;
+    return finalWord;
+}
+
+// TO-DO: Work on ignoring whitespaces
+function backDoorResponse(line) {
+    if (line[length-1] == ' ' || line[length-1] == '') {
+
+    } else {
+        line[length-1];
+    }
+}
